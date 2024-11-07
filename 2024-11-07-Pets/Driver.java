ddcb62c75fb19b30159136860e289d5b400aa0c7
Xinlin Yin <xyin60@stuy.edu>
2024-11-07 10:48:50 -0500

    constructor for Bird class and speak method

--- 2024-11-07-Pets/Driver.java
+++ 2024-11-07-Pets/Driver.java
@@ -1,5 +1,10 @@
 public class Driver{
   public static void main(String[] args){
 
+    Animal human = new Animal("hi", 5, "bob");
+    human.speak();
+
+    Bird pigeon = new Bird("squawk", 3, "billy", 24.5, "white");
+    pigeon.speak();
   }
 }
