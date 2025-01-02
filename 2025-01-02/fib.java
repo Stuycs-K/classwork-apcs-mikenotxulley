2e488e626c2243fbd97c31698f0994db8a52f5d7
Xinlin Yin <xyin60@stuy.edu>
2025-01-02 10:58:21 -0500

    Made fib

--- 2025-01-02/fib.java
+++ 2025-01-02/fib.java
@@ -1,5 +1,8 @@
-public class fib {
-  public static void main(String args[]) {
-    int n1 = 0, n2 = 1, 
-  }
+public int fib(int n) {
+  if (n == 0)
+    return 0;
+  else if(n == 1)
+    return 1;
+  else
+    return fib(n - 1) + fib(n - 2);
 }
